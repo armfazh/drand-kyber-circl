ae8f67a96803d5a5b8ce260b3b4977c82c718bf6
armfazh <armfazh@cloudflare.com>
2023-04-06 05:53:50 -0700

    Implementing G1 group.

--- g1.go
+++ g1.go
@@ -1,16 +1,95 @@
 package drand_kyber_circl
 
 import (
-	E "github.com/cloudflare/circl/ecc/bls12381"
+	"crypto/cipher"
+	"io"
+
+	circl "github.com/cloudflare/circl/ecc/bls12381"
 	"github.com/drand/kyber"
 )
 
-var _ kyber.Group = G1{}
+var _ kyber.SubGroupElement = &G1Elt{}
 
-type G1 struct{}
+type G1Elt struct{ inner circl.G1 }
 
-func (g G1) String() string       { return "bls12-381.G1" }
-func (g G1) ScalarLen() int       { return E.ScalarSize }
-func (g G1) Scalar() kyber.Scalar { return Scalar{} }
-func (g G1) PointLen() int        { return E.G1SizeCompressed }
-func (g G1) Point() kyber.Point   { return nil /*todo*/ }
+func (p *G1Elt) MarshalBinary() (data []byte, err error) { return p.inner.BytesCompressed(), nil }
+
+func (p *G1Elt) UnmarshalBinary(data []byte) error { return p.inner.SetBytes(data) }
+
+func (p *G1Elt) String() string { return p.inner.String() }
+
+func (p *G1Elt) MarshalSize() int { return circl.G1SizeCompressed }
+
+func (p *G1Elt) MarshalTo(w io.Writer) (int, error) {
+	buf, err := p.MarshalBinary()
+	if err != nil {
+		return 0, err
+	}
+	return w.Write(buf)
+}
+
+func (p *G1Elt) UnmarshalFrom(r io.Reader) (int, error) {
+	buf := make([]byte, p.MarshalSize())
+	n, err := io.ReadFull(r, buf)
+	if err != nil {
+		return n, err
+	}
+	return n, p.UnmarshalBinary(buf)
+}
+
+func (p *G1Elt) Equal(p2 kyber.Point) bool { x := p2.(*G1Elt).inner; return p.inner.IsEqual(&x) == 1 }
+
+func (p *G1Elt) Null() kyber.Point { p.inner.SetIdentity(); return p }
+
+func (p *G1Elt) Base() kyber.Point { p.inner = *circl.G1Generator(); return p }
+
+func (p *G1Elt) Pick(rand cipher.Stream) kyber.Point {
+	var buf [32]byte
+	rand.XORKeyStream(buf[:], buf[:])
+	p.inner.Hash(buf[:], nil)
+	return p
+}
+
+func (p *G1Elt) Set(p2 kyber.Point) kyber.Point { p.inner = p2.(*G1Elt).inner; return p }
+
+func (p *G1Elt) Clone() kyber.Point { return new(G1Elt).Set(p) }
+
+func (p *G1Elt) EmbedLen() int {
+	panic("bls12-381: unsupported operation")
+}
+
+func (p *G1Elt) Embed(data []byte, r cipher.Stream) kyber.Point {
+	panic("bls12-381: unsupported operation")
+}
+
+func (p *G1Elt) Data() ([]byte, error) {
+	panic("bls12-381: unsupported operation")
+}
+
+func (p *G1Elt) Add(a, b kyber.Point) kyber.Point {
+	aa := a.(*G1Elt).inner
+	bb := b.(*G1Elt).inner
+	p.inner.Add(&aa, &bb)
+	return p
+}
+
+func (p *G1Elt) Sub(a, b kyber.Point) kyber.Point {
+	p.Neg(b)
+	p.Add(a, p)
+	return p
+}
+
+func (p *G1Elt) Neg(a kyber.Point) kyber.Point {
+	p.Set(a)
+	p.inner.Neg()
+	return p
+}
+
+func (p *G1Elt) Mul(s kyber.Scalar, q kyber.Point) kyber.Point {
+	ss := s.(*Scalar).inner
+	qq := q.(*G1Elt).inner
+	p.inner.ScalarMult(&ss, &qq)
+	return p
+}
+
+func (p *G1Elt) IsInCorrectGroup() bool { return p.inner.IsOnG1() }
